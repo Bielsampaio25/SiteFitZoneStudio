da932a061846eabe2059bd86fc6915fa2a46da08
Bielsampaio25 <gabrielsampaio009officer@gmail.com>
2026-08-04 11:57:47 -0300

    criação dos cards de informação, do grafico financeiro e dos cards de informação financeira.

---
+++ js/dashboardAdmin.js
@@ -0,0 +1,39 @@
+const ctx = document.getElementById("graficoFinanceiro");
+
+new Chart(ctx, {
+    type: "line",
+    data: {
+        labels: [
+
+            "Jan",
+            "Fev",
+            "Mar",
+            "Abr",
+            "Mai",
+            "Jun",
+            "Jul"
+
+        ],
+        datasets: [{
+            label: "Receita (R$)",
+            data: [
+
+                12000,
+                14500,
+                17000,
+                19000,
+                21000,
+                23500,
+                25000
+            ],
+            borderColor:"#FF6A00",
+            backgroundColor:"rgba(255,106,0,0.15)",
+            fill:true,
+            tension:0.4
+        }]
+    },
+    options:{
+        responsive:true,
+        maintainAspectRatio:false
+    }
+});
