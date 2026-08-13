7018a1f88a028afa16eef1c983914ccfeba67b82
Bielsampaio25 <gabrielsampaio009officer@gmail.com>
2026-08-13 11:57:54 -0300

    finalização do model dos funcionarios.

--- java/model/funcionariosModel.java
+++ java/model/funcionariosModel.java
@@ -4,7 +4,7 @@ public class funcionariosModel {
     private String nome;
     private String cpf;
     private String rg;
-    private String dta_nascimento;
+    private String dtaNascimento;
     private String telefone;
     private String email;
     private String sexo;
@@ -15,4 +15,103 @@ public class funcionariosModel {
     private String cep;
     private String bairro;
     private String complemento;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public String getRg(){
+        return rg;
+    }
+
+    public void setRg(String rg){
+        this.rg = rg;
+    }
+    public String getDtaNascimento(){
+        return dtaNascimento;
+    }
+
+    public void setDtaNascimento(String dtaNascimento){
+        this.dtaNascimento = dtaNascimento;
+    }
+    public String getTelefone(){
+        return telefone;
+    }
+
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getSexo(){
+        return sexo;
+    }
+
+    public void setSexo(String sexo){
+        this.sexo = sexo;
+    }
+    public String getSalario(){
+        return salario;
+    }
+
+    public void setSalario(String salario){
+        this.salario = salario;
+    }
+    public String getSituacao(){
+        return situacao;
+    }
+
+    public void setSituacao(String situacao){
+        this.situacao = situacao;
+    }
+    public String getEstado(){
+        return estado;
+    }
+
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+    public String getCidade(){
+        return cidade;
+    }
+
+    public void setCidade(String cidade){
+        this.cidade = cidade;
+    }
+    public String getCep(){
+        return cep;
+    }
+
+    public void setCep(String cep){
+        this.cep = cep;
+    }
+    public String getBairro(){
+        return bairro;
+    }
+
+    public void setBairro(String bairro){
+        this.bairro = bairro;
+    }
+    public String getComplemento(){
+        return complemento;
+    }
+
+    public void setComplemento(String complemento){
+        this.complemento = complemento;
+    }
 }
